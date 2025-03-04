bb1892e04618c16745d24ff39fc1aecc9931f138
olemartin25sfj <olemartin25fj@gmail.com>
2025-03-04 13:03:08 +0100

    ArrayList

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add(1);
+        arrayList.Add("Hei");
+        arrayList.Add(3.14);
+        arrayList.Add(true);
+
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
