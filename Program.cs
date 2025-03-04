2dcd1208738832dd3de1bf87a2cf304ea4f65b9a
olemartin25sfj <olemartin25fj@gmail.com>
2025-03-04 13:09:50 +0100

    Fooling around

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main()
     {
-        ArrayList arrayList = new ArrayList();
-        arrayList.Add(1);
-        arrayList.Add("Hei");
-        arrayList.Add(3.14);
-        arrayList.Add(true);
+        // ArrayList arrayList = new ArrayList();
+        // arrayList.Add(1);
+        // arrayList.Add("Hei");
+        // arrayList.Add(3.14);
+        // arrayList.Add(true);
 
-        foreach (var item in arrayList)
+        // foreach (var item in arrayList)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        foreach (var item in list)
         {
             Console.WriteLine(item);
         }
+
+        // Console.WriteLine(x);
+
+        // int x = 5;
     }
 }
